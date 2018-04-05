3c45f36f31168b58c242b4910a9f356c10a13fa7
ltlg <36575603+ltlg@users.noreply.github.com>
2018-04-05 23:45:11 +1200

    Add files via upload

---
+++ WebApiTestClient/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTestClient.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        [Authorize]
+        [HttpGet("Protected")]
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<object> Protected()
+        {
+            return "Protected area";
+        }
+    }
+}
