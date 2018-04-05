ea20c3979a2426119def52bd129dea692919cd41
ltlg <36575603+ltlg@users.noreply.github.com>
2018-04-05 23:52:11 +1200

    Add files via upload

---
+++ AuthenticationServer/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Primitives;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Api")]
+    public class ApiController : Controller
+    {
+        [Authorize, HttpGet("~/api/test")]
+        public IActionResult GetMessage()
+        {
+            return Json(new
+            {
+                Subject = User.GetClaim(OpenIdConnectConstants.Claims.Subject),
+                Name = User.Identity.Name
+            });
+        }
+    }
+}
---
+++ AuthenticationServer/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthenticationServer.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
